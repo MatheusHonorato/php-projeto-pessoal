0a134fb1a454ae773791bfe16594c28d54f346f6
Matheus <matheus@LAPTOP-5QQPHSHE.localdomain>
2023-03-31 09:38:55 -0300

    up routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -11,22 +11,22 @@ use App\Repositories\UserRepository;
 
 try {
     $userRepository = new UserRepository(queryBuilder: $queryBuilder);
-    $requestModel = new RequestModelUser(validator: $http->validator, model: UserModel::class);
+    $requestModelUser = new RequestModelUser(validator: $http->validator, model: UserModel::class);
 
     $companyRepository = new CompanyRepository(queryBuilder: $queryBuilder);
-    $requestModel = new RequestModelCompany(validator: $http->validator, model: CompanyModel::class);
+    $requestModelCompany = new RequestModelCompany(validator: $http->validator, model: CompanyModel::class);
 
-    $router->add(uri: '/users', method: 'GET', controller: 'UsersController:index', repository: $userRepository, requestModel: $requestModel);
-    $router->add(uri: '/users/(:numeric)', method: 'GET', controller: 'UsersController:getById', repository: $userRepository, requestModel: $requestModel, wildcardAliases: ['id']);
-    $router->add(uri: '/users', method: 'POST', controller: 'UsersController:store', repository: $userRepository, requestModel: $requestModel);
-    $router->add(uri: '/users/(:numeric)', method: 'PUT', controller: 'UsersController:update', repository: $userRepository, requestModel: $requestModel, wildcardAliases: ['id']);
-    $router->add(uri: '/users/(:numeric)', method: 'DELETE', controller: 'UsersController:destroy', repository: $userRepository, requestModel: $requestModel, wildcardAliases: ['id']);
+    $router->add(uri: '/users', method: 'GET', controller: 'UsersController:index', repository: $userRepository, requestModel: $requestModelUser);
+    $router->add(uri: '/users/(:numeric)', method: 'GET', controller: 'UsersController:getById', repository: $userRepository, requestModel: $requestModelUser, wildcardAliases: ['id']);
+    $router->add(uri: '/users', method: 'POST', controller: 'UsersController:store', repository: $userRepository, requestModel: $requestModelUser);
+    $router->add(uri: '/users/(:numeric)', method: 'PUT', controller: 'UsersController:update', repository: $userRepository, requestModel: $requestModelUser, wildcardAliases: ['id']);
+    $router->add(uri: '/users/(:numeric)', method: 'DELETE', controller: 'UsersController:destroy', repository: $userRepository, requestModel: $requestModelUser, wildcardAliases: ['id']);
 
-    $router->add(uri: '/companies', method: 'GET', controller: 'CompaniesController:index', repository: $companyRepository, requestModel: $requestModel);
-    $router->add(uri: '/companies/(:numeric)', method: 'GET', controller: 'CompaniesController:getById', repository: $companyRepository, requestModel: $requestModel, wildcardAliases: ['id']);
-    $router->add(uri: '/companies', method: 'POST', controller: 'CompaniesController:store', repository: $companyRepository, requestModel: $requestModel);
-    $router->add(uri: '/companies/(:numeric)', method: 'PUT', controller: 'CompaniesController:update', repository: $companyRepository, requestModel: $requestModel, wildcardAliases: ['id']);
-    $router->add(uri: '/companies/(:numeric)', method: 'DELETE', controller: 'CompaniesController:destroy', repository: $companyRepository, requestModel: $requestModel, wildcardAliases: ['id']);
+    $router->add(uri: '/companies', method: 'GET', controller: 'CompaniesController:index', repository: $companyRepository, requestModel: $requestModelCompany);
+    $router->add(uri: '/companies/(:numeric)', method: 'GET', controller: 'CompaniesController:getById', repository: $companyRepository, requestModel: $requestModelCompany, wildcardAliases: ['id']);
+    $router->add(uri: '/companies', method: 'POST', controller: 'CompaniesController:store', repository: $companyRepository, requestModel: $requestModelCompany);
+    $router->add(uri: '/companies/(:numeric)', method: 'PUT', controller: 'CompaniesController:update', repository: $companyRepository, requestModel: $requestModelCompany, wildcardAliases: ['id']);
+    $router->add(uri: '/companies/(:numeric)', method: 'DELETE', controller: 'CompaniesController:destroy', repository: $companyRepository, requestModel: $requestModelCompany, wildcardAliases: ['id']);
 
     $router->init();
 } catch (Exception $e) {

bee2144a3b8d871db81ad0e3d2501960252362e0
Matheus <matheus@LAPTOP-5QQPHSHE.localdomain>
2023-03-31 09:37:23 -0300

    up routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -13,14 +13,14 @@ try {
     $userRepository = new UserRepository(queryBuilder: $queryBuilder);
     $requestModel = new RequestModelUser(validator: $http->validator, model: UserModel::class);
 
+    $companyRepository = new CompanyRepository(queryBuilder: $queryBuilder);
+    $requestModel = new RequestModelCompany(validator: $http->validator, model: CompanyModel::class);
+
     $router->add(uri: '/users', method: 'GET', controller: 'UsersController:index', repository: $userRepository, requestModel: $requestModel);
     $router->add(uri: '/users/(:numeric)', method: 'GET', controller: 'UsersController:getById', repository: $userRepository, requestModel: $requestModel, wildcardAliases: ['id']);
     $router->add(uri: '/users', method: 'POST', controller: 'UsersController:store', repository: $userRepository, requestModel: $requestModel);
     $router->add(uri: '/users/(:numeric)', method: 'PUT', controller: 'UsersController:update', repository: $userRepository, requestModel: $requestModel, wildcardAliases: ['id']);
     $router->add(uri: '/users/(:numeric)', method: 'DELETE', controller: 'UsersController:destroy', repository: $userRepository, requestModel: $requestModel, wildcardAliases: ['id']);
-
-    $companyRepository = new CompanyRepository(queryBuilder: $queryBuilder);
-    $requestModel = new RequestModelCompany(validator: $http->validator, model: CompanyModel::class);
 
     $router->add(uri: '/companies', method: 'GET', controller: 'CompaniesController:index', repository: $companyRepository, requestModel: $requestModel);
     $router->add(uri: '/companies/(:numeric)', method: 'GET', controller: 'CompaniesController:getById', repository: $companyRepository, requestModel: $requestModel, wildcardAliases: ['id']);
